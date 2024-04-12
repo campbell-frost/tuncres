ea86934acb9cd2c32b54fb2bc039ea915f6e2eda
campbell-frost <campbellsfrost@gmail.com>
2024-04-12 00:35:33 -0400

    created navbar

---
+++ src/app/ui/navbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Navbar() {
+    return (
+        <div className="bg-slate-900 max-w-screen w-full flex justify-center py-10">
+            <div className="pl-4"> 
+                <h1 className="bg-gradient-to-r from-green-500 to-cyan-400 inline-block text-transparent bg-clip-text tracking-widest text-9xl">TUNCRES</h1>
+            </div>
+        </div>
+    );
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,13 +7,15 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
+    letterSpacing: {
+      tightest: '-.075em',
+      tighter: '-.05em',
+      tight: '-.025em',
+      normal: '0',
+      wide: '.025em',
+      wider: '.05em',
+      widest: '.5em',
+    }
   },
   plugins: [],
 };
