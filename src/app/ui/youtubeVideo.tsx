103277d28919b3e90b9c95cf8ec00ce941658242
campbell-frost <campbellsfrost@gmail.com>
2024-04-13 13:24:46 -0400

    fixed video errors

--- src/app/ui/youtubeVideo.tsx
+++ src/app/ui/youtubeVideo.tsx
@@ -8,14 +8,14 @@ interface youtubeVideoProps {
 export default function YoutubeVideo({ link, title }: youtubeVideoProps) {
     return (
         <div className='responsive'>
-            <iframe 
-            width="560"
-            height="315" 
-            src={link}
-            title="YouTube video player"
-            frameborder="0" 
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-            referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            <iframe
+                width="560"
+                height="315"
+                src={link}
+                title="YouTube video player"
+
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            ></iframe>
 
         </div>
 
