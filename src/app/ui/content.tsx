60f04187ffda0cd9954634933e484011577b2978
campbell-frost <campbellsfrost@gmail.com>
2024-04-22 00:07:14 -0400

    hair update

--- src/app/ui/content.tsx
+++ src/app/ui/content.tsx
@@ -5,9 +5,10 @@ export default function Content() {
     return (
         <div className="flex items-center justify-center mt-40 mx-80">
             <div className="m-20">
-                <p >David sucks</p>
-                <p >Michael is cooler</p>
-                <p >Campbell is the coolest</p>
+                <p>David sucks</p>
+                <p>Michael is cooler</p>
+                <p>Campbell is the coolest</p>
+                <p>Michael has the worst hair.</p>
             </div>
         </div>
     );
